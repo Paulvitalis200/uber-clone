bccc2a7606ad9325eaf8d12471657c171d0a7e1a
Paul Otieno <paul.otieno@gitlab.com>
2025-07-09 14:31:47 +0300

    Setup eslint and prettier

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-      <SafeAreaView className="flex-1 items-center justify-center bg-white">
-        <Text className="text-xl font-bold text-blue-500">
-          Welcome to Nativewind!
-        </Text>
-      </SafeAreaView>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl">Uber Clone!</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
